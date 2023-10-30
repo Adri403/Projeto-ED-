2ad43a4c182a3ad4066a4a6216524880630c2c11
Adriel <adrielisaias02@gmail.com>
2023-10-30 16:02:09 -0300

    adicionando algo

---
+++ meu_grafo_lista_adj.py
@@ -0,0 +1,159 @@
+from bibgrafo.grafo_lista_adjacencia import GrafoListaAdjacencia
+from bibgrafo.grafo_errors import *
+
+
+class MeuGrafo(GrafoListaAdjacencia):
+
+    def vertices_nao_adjacentes(self):
+        '''
+        Provê um conjunto de vértices não adjacentes no grafo.
+        O conjunto terá o seguinte formato: {X-Z, X-W, ...}
+        Onde X, Z e W são vértices no grafo que não tem uma aresta entre eles.
+        :return: Um objeto do tipo set que contém os pares de vértices não adjacentes
+        '''
+        arestas = self.arestas
+        dict_adjacentes = dict()
+
+        for k in arestas:
+            v1 = str(arestas[k].v1)
+            v2 = str(arestas[k].v2)
+
+            if v1 not in dict_adjacentes:
+                dict_adjacentes[v1] = []
+
+            if v2 not in dict_adjacentes[v1]:
+                dict_adjacentes[v1].append(v2)
+
+            if v2 not in dict_adjacentes:
+                dict_adjacentes[v2] = []
+
+            if v1 not in dict_adjacentes[v2]:
+                dict_adjacentes[v2].append(v1)
+        
+        vertices = set(dict_adjacentes.keys())
+        vertices_nao_adjacentes = set()
+
+        for v1 in vertices:
+            for v2 in vertices:
+                if v1 != v2 and v2 not in dict_adjacentes[v1]:
+                    inverso = f"{v1}-{v2}"
+                    par = f"{v2}-{v1}"
+
+                    if par not in vertices_nao_adjacentes and inverso not in vertices_nao_adjacentes:
+                        vertices_nao_adjacentes.add(par)
+
+        return vertices_nao_adjacentes
+        
+
+    def ha_laco(self):
+        '''
+        Verifica se existe algum laço no grafo.
+        :return: Um valor booleano que indica se existe algum laço.
+        '''
+        arestas = self.arestas
+
+        for x in arestas:
+           if arestas[x].v1 == arestas[x].v2:
+               return True, "existe laço no grafo"
+        
+        return False, "não existe laço no grafo"
+
+    def grau(self, V):
+        '''
+        Provê o grau do vértice passado como parâmetro
+        :param V: O rótulo do vértice a ser analisado
+        :return: Um valor inteiro que indica o grau do vértice
+        :raises: VerticeInvalidoError se o vértice não existe no grafo
+        '''
+        arestas = self.arestas
+        vertices = self.vertices
+        vertices_string = list()
+        contador = 0
+
+        for c in range(len(vertices)):
+            vertices_string.append(str(vertices[c]))
+
+        if V in vertices_string:
+            for k in arestas:
+                if V == str(arestas[k].v1) or V == str(arestas[k].v2):
+                    contador += 1
+
+            return contador
+        else:
+            return "Este vértice não existe no grafo"
+
+    def ha_paralelas(self):
+        '''
+        Verifica se há arestas paralelas no grafo
+        :return: Um valor booleano que indica se existem arestas paralelas no grafo.
+        '''
+        arestas = self.arestas
+
+        for k in arestas:
+            for l in arestas:
+                if k == l:
+                    continue
+                else:
+                    if (arestas[k].v1 == arestas[l].v1 and arestas[k].v2 == arestas[l].v2) or (arestas[k].v1 == arestas[l].v2 and arestas[k].v2 == arestas[l].v1):
+                        return True, "existem paralelas no grafo"
+                    
+        return False, "não existem paralelas no grafo"
+
+    def arestas_sobre_vertice(self, V):
+        '''
+        Provê uma lista que contém os rótulos das arestas que incidem sobre o vértice passado como parâmetro
+        :param V: Um string com o rótulo do vértice a ser analisado
+        :return: Uma lista os rótulos das arestas que incidem sobre o vértice
+        :raises: VerticeInvalidoException se o vértice não existe no grafo
+        '''
+        arestas = self.arestas
+        vertices = self.vertices
+        vertices_string = list()
+        lista_arestas = list()
+
+        for c in range(len(vertices)):
+            vertices_string.append(str(vertices[c]))
+
+        if V in vertices_string:
+            for k in arestas:
+                if V == str(arestas[k].v1) or V == str(arestas[k].v2):
+                    lista_arestas.append(k)
+
+            return lista_arestas
+        else:
+            return "Este vértice não existe no grafo"
+
+    def eh_completo(self):
+        '''
+        Verifica se o grafo é completo.
+        :return: Um valor booleano que indica se o grafo é completo
+        '''
+        passarestas = self.arestas
+        vertices = self.vertices
+        dict_adjacentes = dict()
+
+        for k in arestas:
+            v1 = str(arestas[k].v1)
+            v2 = str(arestas[k].v2)
+
+            if v1 not in dict_adjacentes:
+                dict_adjacentes[v1] = []
+
+            if v2 not in dict_adjacentes[v1]:
+                dict_adjacentes[v1].append(v2)
+
+            if v2 not in dict_adjacentes:
+                dict_adjacentes[v2] = []
+
+            if v1 not in dict_adjacentes[v2]:
+                dict_adjacentes[v2].append(v1)
+
+        for j in dict_adjacentes:
+            for s in range(len(vertices)):
+                if str(vertices[s]) == j:
+                    continue
+                else:
+                    if str(vertices[s]) not in dict_adjacentes[j]:
+                        return False, "não é completo"
+                
+        return True, "é completo"
--- teste.py
+++ teste.py
@@ -1,7 +1,7 @@
-import bibgrafo.grafo_lista_adjacencia
-from bibgrafo.aresta import Aresta
+from bibgrafo.grafo_lista_adjacencia import GrafoListaAdjacencia
+from meu_grafo_lista_adj import MeuGrafo
 
-paraiba = bibgrafo.grafo_lista_adjacencia.GrafoListaAdjacencia()
+paraiba = GrafoListaAdjacencia()
 
 paraiba.adiciona_vertice("J")
 paraiba.adiciona_vertice("C")
@@ -11,15 +11,22 @@ paraiba.adiciona_vertice("M")
 paraiba.adiciona_vertice("T")
 paraiba.adiciona_vertice("Z")
 
-paraiba.adiciona_aresta("a1", "J", "C")
-paraiba.adiciona_aresta("a2", "C", "E")
-paraiba.adiciona_aresta("a3", "C", "E")
-paraiba.adiciona_aresta("a4", "C", "P")
-paraiba.adiciona_aresta("a5", "C", "P")
-paraiba.adiciona_aresta("a6", "C", "M")
-paraiba.adiciona_aresta("a7", "C", "T")
-paraiba.adiciona_aresta("a8", "M", "T")
-paraiba.adiciona_aresta("a9", "T", "Z")
+paraiba.adiciona_aresta('a1', 'J', 'C')
+paraiba.adiciona_aresta('a2', 'C', 'E')
+paraiba.adiciona_aresta('a3', 'C', 'Z')
+paraiba.adiciona_aresta('a4', 'C', 'M')
+paraiba.adiciona_aresta('a5', 'C', 'P')
+paraiba.adiciona_aresta('a6', 'C', 'M')
+paraiba.adiciona_aresta('a7', 'C', 'T')
+paraiba.adiciona_aresta('a8', 'M', 'T')
+paraiba.adiciona_aresta('a9', 'T', 'Z')
 
+print(MeuGrafo.vertices_nao_adjacentes(paraiba))
+print(*MeuGrafo.ha_laco(paraiba))
+print(*MeuGrafo.ha_paralelas(paraiba))
+print(*MeuGrafo.eh_completo(paraiba))
 
-print(paraiba)
+V = input()
+
+print(MeuGrafo.grau(paraiba, V))
+print(MeuGrafo.arestas_sobre_vertice(paraiba, V))
